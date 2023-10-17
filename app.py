0e65940b9c8c2af12d427088ca6508d038467f3e
LoadingStill <loadingstill@noreply.localhost>
2023-10-17 17:03:06 -0500

    Upload files to "/"

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    # You can add any additional logic here if needed.
+    return render_template('home.html')
+
+
+@app.route('/games/csgo2')
+def csgo2_page():
+    return render_template('games/csgo2.html')
+
+
+@app.route('/games/factorio')
+def factorio_page():
+    return render_template('games/factorio.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=8000)
