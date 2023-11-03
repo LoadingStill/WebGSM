0be44e08ac038c646d22c3c4d3e52025b033273a
LoadingStill <loadingstill@noreply.localhost>
2023-11-02 22:19:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import subprocess
+import os
+
 
 app = Flask(__name__)
 
@@ -17,6 +20,17 @@ def cs2_page():
 @app.route('/games/factorio')
 def factorio_page():
     return render_template('games/factorio.html')
+
+
+@app.route('/run-script', methods=['POST'])
+def run_script():
+    script_path = os.path.join(os.path.dirname(__file__), 'gamesFolder', 'factorio', 'factorioInstall.sh')
+    try:
+        # Run the shell script and capture the output
+        output = subprocess.check_output([script_path], stderr=subprocess.STDOUT, shell=True)
+        return output.decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e.output.decode('utf-8')}"
 
 
 if __name__ == '__main__':
