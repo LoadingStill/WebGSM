8a01e7b9696354fe2a68e46a7fd6f899712e122a
LoadingStill <ryan.young@howtoit.com>
2023-11-05 01:23:37 -0400

    updated #comments and the description of the .service file

--- LGSM-WebGui-Installer.sh
+++ LGSM-WebGui-Installer.sh
@@ -9,10 +9,16 @@ GITEA_FILE_URL="https://git.howtoit.com/LoadingStill/LGSM-WebGUI/archive/main.zi
 # Make the folder where this will be stored
 mkdir /var/www
 
+# Download file to /var/www folder
 wget -O /var/www/lgsm-webgui.zip https://git.howtoit.com/LoadingStill/LGSM-WebGUI/archive/main.zip
 
-unzip /var/www/lgsm-webgui.zip /var/www/
+# Unzip file
+unzip /var/www/lgsm-webgui.zip -d /var/www/
 
+# Remove no longer needed zip file
+rm /var/www/lgsm-webgui.zip
+
+# Tells user what has completed
 echo "Download, unzip, and move completed."
 
 
@@ -20,7 +26,7 @@ echo "Download, unzip, and move completed."
 # Create the systemd service unit file
 cat <<EOL > /etc/systemd/system/lgsm-webgui.service
 [Unit]
-Description=Download and Install Script for $(whoami)
+Description=LGMS-WebGUI start at boot.
 
 [Service]
 Type=simple
@@ -40,4 +46,4 @@ systemctl enable lgsm-webgui.service
 # Start the service
 systemctl start lgsm-webgui.service
 
-echo "Setup completed."
+echo "Start at boot enabled."
