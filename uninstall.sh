fa4893e28ae72f8d0893dc98183edd1cbe6dea19
LoadingStill <ryan.young@howtoit.com>
2023-11-05 01:14:52 -0400

    added an uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This scrip will not remove game servers, you will need to do that manually through the gui or terminal.
+
+# Stop the application from starting at boot
+sudo systemctl disable lgsm-webgui.service
+
+# Stop the application from running
+sudo systemctl stop lgsm-webgui.service
+
+# Remove the file that allows start at boot
+sudo rm /etc/systemd/system/lgsm-webgui.service
+
+# Reload systemctl daemon
+sudo systemctl daemon-reload
+
+# Remove the webgui files
+sudo rm -r /var/www/lgsm-webgui
