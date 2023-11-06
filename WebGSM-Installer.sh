25d82f23e3e65c9f59ee47c034f15cbc24e6e5f6
LoadingStill <loadingstill@noreply.localhost>
2023-11-06 17:36:23 -0600

    Update WebGSM-Installer.sh

--- WebGSM-Installer.sh
+++ WebGSM-Installer.sh
@@ -15,28 +15,28 @@ GREEN="\033[32m"
 RESET="\033[0m"
 
 # Define the URL of the file you want to download from Gitea
-GITEA_FILE_URL="https://git.howtoit.com/LoadingStill/LGSM-WebGUI/archive/main.zip"
+GITEA_FILE_URL="https://git.howtoit.com/LoadingStill/WebGSM/archive/main.zip"
 
 # Make the folder where this will be stored
 mkdir /var/www
 
 # Download file to /var/www folder
-wget -O /var/www/lgsm-webgui.zip https://git.howtoit.com/LoadingStill/LGSM-WebGUI/archive/main.zip
+wget -O /var/www/WebGSM.zip https://git.howtoit.com/LoadingStill/WebGSM/archive/main.zip
 
 # Unzip file
-unzip /var/www/lgsm-webgui.zip -d /var/www/
+unzip /var/www/WebGSM.zip -d /var/www/
 
 # Remove no longer needed zip file
-rm /var/www/lgsm-webgui.zip
+rm /var/www/WebGSM.zip
 
 # Tells user what has completed
 echo -e ${GREEN}"Download, unzip, and move completed."${RESET}
 
 # Create a virtual environment
-python3 -m venv /var/www/lgsm-webgui/venv
+python3 -m venv /var/www/WebGSM/venv
 
 # Activate the virtual environment
-source /var/www/lgsm-webgui/venv/bin/activate
+source /var/www/WebGSM/venv/bin/activate
 
 # Install the psutil module
 pip install psutil
@@ -44,24 +44,24 @@ pip install psutil
 # Exit the virtual environment
 deactivate
 
-# make /var/www/lgsm-webgui/run.sh executable
-sudo chmod +x /var/www/lgsm-webgui/run.sh
+# make /var/www/WebGSM/run.sh executable
+sudo chmod +x /var/www/WebGSM/run.sh
 
 # Adds user to nologin group
 sudo useradd -r -s /usr/sbin/nologin $NEW_USER
 
 # Change ownership of file to group lgms-webgui
-sudo chown -R $NEW_USER:$NEW_USER /var/www/lgsm-webgui
+sudo chown -R $NEW_USER:$NEW_USER /var/www/WebGSM
 
 # Create the systemd service unit file
-cat <<EOL > /etc/systemd/system/lgsm-webgui.service
+cat <<EOL > /etc/systemd/system/WebGSM.service
 [Unit]
 Description=LGMS-WebGUI start at boot.
 
 [Service]
 Type=simple
 User=$NEW_USER
-ExecStart=/var/www/lgsm-webgui/run.sh
+ExecStart=/var/www/WebGSM/run.sh
 
 [Install]
 WantedBy=multi-user.target
@@ -71,15 +71,15 @@ EOL
 systemctl daemon-reload
 
 # Enable the service to start on boot
-systemctl enable lgsm-webgui.service
+systemctl enable WebGSM.service
 
 # Start the service
-systemctl start lgsm-webgui.service
+systemctl start WebGSM.service
 
 echo -e ${GREEN}"Start at boot enabled." ${RESET}
 
 # Set the directory where you want to search for .sh files
-directory="/var/www/lgsm-webgui"
+directory="/var/www/WebGSM"
 
 # Use the find command with sudo to locate all .sh files and make them executable
 # This allows execution of all .sh files for game instlation and edditing.
