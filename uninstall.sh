b2e90fa7651d9fa1df2a1604fcb6538b050e5d86
LoadingStill <loadingstill@noreply.localhost>
2023-11-07 13:11:00 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,19 +2,19 @@
 # This scrip will not remove game servers, you will need to do that manually through the gui or terminal.
 
 # Stop the application from starting at boot
-sudo systemctl disable lgsm-webgui.service
+sudo systemctl disable WebGSM.service
 
 # Stop the application from running
-sudo systemctl stop lgsm-webgui.service
+sudo systemctl stop WebGSM.service
 
 # Remove the file that allows start at boot
-sudo rm /etc/systemd/system/lgsm-webgui.service
+sudo rm /etc/systemd/system/WebGSM.service
 
 # Reload systemctl daemon
 sudo systemctl daemon-reload
 
 # Remove the webgui files
-sudo rm -r /var/www/lgsm-webgui
+sudo rm -r /var/www/WebGSM
 
 # Removed the initial isntall script
-sudo rm /$SUDO_USER/Downloads/LGSM-WebGUI-Installer.sh
+sudo rm /$SUDO_USER/Downloads/WebGSM-Installer.sh
