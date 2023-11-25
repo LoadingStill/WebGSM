ee325ead6011ab957c1de4f0f3f31dab562f3820
Ryan Young <youngrya001@gmail.com>
2023-11-25 13:00:33 -0500

    Sudo code/actual code for installing MCJava server, writing out what I need to do to complete this task

--- games/minecraftjava/installMinecraftJava.sh
+++ games/minecraftjava/installMinecraftJava.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#adduser mcserver
-#su - mcserver
-#wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
-#./mcserver install
 
-sudo apt install neofetch htop -y
+# Sudo code/actual code for installing MCJava server
+
+#add mcserver user requires sudo, requires password to be typed
+# sudo adduser mcserver
+
+#add new user to sudo group for install to automate the process more
+# sudo usermod -aG sudo mcserver
+
+#login as mcserver user, requires password to be typed
+#su - mcserver
+
+#Installs LinuxGSM and installs minecraft java server.
+#wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
+
+#Installs
+#./mcserver install
