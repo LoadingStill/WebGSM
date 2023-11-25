f488577d8f005b2a26d44ccc2bc4424ec6ca6a39
Ryan Young <youngrya001@gmail.com>
2023-11-25 13:04:28 -0500

    uncomment for manual test

--- games/minecraftjava/installMinecraftJava.sh
+++ games/minecraftjava/installMinecraftJava.sh
@@ -3,16 +3,16 @@
 # Sudo code/actual code for installing MCJava server
 
 #add mcserver user requires sudo, requires password to be typed
-# sudo adduser mcserver
+sudo adduser mcserver
 
 #add new user to sudo group for install to automate the process more
-# sudo usermod -aG sudo mcserver
+sudo usermod -aG sudo mcserver
 
 #login as mcserver user, requires password to be typed
-#su - mcserver
+su - mcserver
 
 #Installs LinuxGSM and installs minecraft java server.
-#wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
+wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
 
 #Installs
-#./mcserver install
+./mcserver install
