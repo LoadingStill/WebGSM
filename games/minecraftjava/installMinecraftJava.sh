3f068e815363469fc43df30ddc7cef558a437dd3
LoadingStill <loadingstill@noreply.localhost>
2023-11-26 00:36:21 -0600

    Update games/minecraftjava/installMinecraftJava.sh

--- games/minecraftjava/installMinecraftJava.sh
+++ games/minecraftjava/installMinecraftJava.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Sudo code/actual code for installing MCJava server
+# Create a new user named "debian" with the password "1234567890"
+sudo useradd -m debian
+echo "debian:1234567890" | sudo chpasswd
 
-#add mcserver user requires sudo, requires password to be typed
-sudo adduser mcserver
+# Add the user to the sudo group
+sudo usermod -aG sudo debian
 
-#add new user to sudo group for install to automate the process more
-sudo usermod -aG sudo mcserver
+# Switch to the new user
+su - debian
 
-#login as mcserver user, requires password to be typed
-su - mcserver
-
-#Installs LinuxGSM and installs minecraft java server.
+# Download LinuxGSM script and install Minecraft server
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver
 
 # Installs game dependencies
 sudo nala install bc binutils bsdmainutils bzip2 ca-certificates cpio curl distro-info file gzip hostname jq lib32gcc-s1 lib32stdc++6 netcat-openbsd openjdk-17-jre python3 tar tmux unzip util-linux uuid-runtime wget xz-utils -y
 
-#Installs
+# Install Miencraft Java Server
 ./mcserver install
