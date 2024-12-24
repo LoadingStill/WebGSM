6b116bd8f850a9360ce11a2a9f94ea7d3d053754
Ryan Young <youngrya001@gmail.com>
2024-12-24 11:03:09 -0600

    Update WebGSM-docker-install.sh
    docker file removed sudo commands

--- WebGSM-docker-install.sh
+++ WebGSM-docker-install.sh
@@ -5,12 +5,12 @@
 # Installs the needed software
 apt update
 dpkg --add-architecture i386
-sudo apt install nala -y
-sudo nala install python3-venv gcc python3-dev python3-pip unzip curl -y
+apt install nala -y
+nala install python3-venv gcc python3-dev python3-pip unzip curl -y
 # LinuxGSM dependencies
 sudo nala install bc binutils bsdmainutils bzip2 ca-certificates cpio curl distro-info file gzip hostname jq lib32gcc-s1 lib32stdc++6 netcat-openbsd python3 tar tmux unzip util-linux uuid-runtime wget xz-utils -y
 
-sudo nala update
+nala update
 #pip install --no-binary :all: psutil
 
 # variables
@@ -22,7 +22,7 @@ GREEN="\033[32m"
 RESET="\033[0m"
 
 # Make the folder where this will be stored
-sudo mkdir /var/www
+mkdir /var/www
 
 # Download the latest version of the WebGSM repository from GitHub as a zip file and save it to /tmp/latest.zip
 # Unzip the downloaded file into /tmp/
@@ -35,13 +35,13 @@ echo -e "${GREEN}Download, unzip, and move completed.${RESET}"
 
 
 # make /var/www/WebGSM/run.sh executable
-sudo chmod +x /var/www/webgsm/run.sh
+chmod +x /var/www/webgsm/run.sh
 
 # Adds user to no-login group
-sudo useradd -r -s /usr/sbin/nologin "$NEW_USER"
+useradd -r -s /usr/sbin/nologin "$NEW_USER"
 
 # Change ownership of file to group lgms-webgui
-sudo chown -R "$NEW_USER:$NEW_USER" /var/www/webgsm
+chown -R "$NEW_USER:$NEW_USER" /var/www/webgsm
 
 
 # Create the systemd service unit file
@@ -59,13 +59,13 @@ WantedBy=multi-user.target
 EOL
 
 # Reload the systemd manager to recognize the new service
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 # Enable the service to start on boot
-sudo systemctl enable webgsm.service
+systemctl enable webgsm.service
 
 # Start the service
-sudo systemctl start webgsm.service
+systemctl start webgsm.service
 
 echo -e "${GREEN}Start at boot enabled. ${RESET}"
 
@@ -74,7 +74,7 @@ directory="/var/www/webgsm"
 
 # Use the find command with sudo to locate all .sh files and make them executable
 # This allows execution of all .sh files for game installation and editing.
-sudo find "$directory" -type f -name "*.sh" -exec sudo chmod +x {} \;
+find "$directory" -type f -name "*.sh" -exec sudo chmod +x {} \;
 
 # Sets files in webgsm directory to run sudo commands with out passwords
 echo '# Sets script to not need a password to install.  This is only for the webgsm files.' | sudo tee -a /etc/sudoers
