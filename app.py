6e09fb256e93e26a2a8668038c5c343cbe374ec8
LoadingStill <youngrya001@gmail.com>
2024-12-29 10:56:06 -0600

    dynamic game routes

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# List of game names (without the .html extension)
+game_pages = ['cs2', 'arma3', 'dst', 'factorio', 'eco', 'minecraftJava']
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', game_pages=game_pages)
 
-@app.route('/games/cs2.html')
-def cs2():
-    return render_template('/games/cs2.html')  # Ensure this template exists in the templates folder
+@app.route('/games/<game_name>.html')
+def game(game_name):
+    if game_name in game_pages:
+        return render_template(f'/games/{game_name}.html')
+    else:
+        return "Game not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
