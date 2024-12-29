be9eb7f10aa85f1860d211b8d2c7adaaa2987384
LoadingStill <youngrya001@gmail.com>
2024-12-29 12:09:56 -0600

    file clean up

--- oldGSM/games/factorio/installFactorio.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd
-sudo apt update
-sudo apt insatll neofetch -y
--- oldGSM/static/js/ram_usage.js
+++
@@ -1,8 +0,0 @@
-function updateRAMUsage() {
-    $.get('/get_ram_usage', function(data) {
-        // Assuming the server returns RAM usage as a percentage (e.g., data.ram_usage = 45.67 for 45.67%)
-        $('#ram-usage').text(data.ram_usage.toFixed(2) + '%');
-    });
-}
-
-setInterval(updateRAMUsage, 1000); // Update RAM usage every 5 seconds
