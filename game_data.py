67e75455efcc2a15dd475355eaf9a370bcefceaf
LoadingStill <youngrya001@gmail.com>
2024-12-29 18:42:17 -0600

    cleaned up dont starve together

--- game_data.py
+++ game_data.py
@@ -1,6 +1,7 @@
 game_pages = {
     'cs2': '/static/images/cs2.jpg',
     'fctr': '/static/images/fctr.jpg',
-    'arma3': '/static/images/arma3.jpg'
+    'arma3': '/static/images/arma3.jpg',
+    'dst': '/static/images/dst.jpg'
     # Add more games here as needed
 }
