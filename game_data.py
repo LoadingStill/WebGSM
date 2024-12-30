38924f4f12d5781039ef1e016b400404133734b8
LoadingStill <youngrya001@gmail.com>
2024-12-29 18:38:13 -0600

    cleaneed up the arma2 page

--- game_data.py
+++ game_data.py
@@ -1,5 +1,6 @@
 game_pages = {
     'cs2': '/static/images/cs2.jpg',
-    'fctr': '/static/images/fctr.jpg'
+    'fctr': '/static/images/fctr.jpg',
+    'arma3': '/static/images/arma3.jpg'
     # Add more games here as needed
 }
