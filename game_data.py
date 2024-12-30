192133f8fa36faecbee39f67f2f2dc48bb92656d
LoadingStill <youngrya001@gmail.com>
2024-12-29 18:45:12 -0600

    cleaned up eco

--- game_data.py
+++ game_data.py
@@ -2,6 +2,7 @@ game_pages = {
     'cs2': '/static/images/cs2.jpg',
     'fctr': '/static/images/fctr.jpg',
     'arma3': '/static/images/arma3.jpg',
-    'dst': '/static/images/dst.jpg'
+    'dst': '/static/images/dst.jpg',
+    'eco': '/static/images/eco.jpg',
     # Add more games here as needed
 }
