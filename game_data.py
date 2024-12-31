b71dc3420f82e1a8b5052dc248c8485d8d73e4d9
HomeMadeGames Studio <youngrya001@gmail.com>
2024-12-30 21:46:25 -0600

    ABC order

--- game_data.py
+++ game_data.py
@@ -1,8 +1,8 @@
 game_pages = {
-    'cs2': '/static/images/cs2.jpg',
-    'fctr': '/static/images/fctr.jpg',
     'arma3': '/static/images/arma3.jpg',
+    'cs2': '/static/images/cs2.jpg',
     'dst': '/static/images/dst.jpg',
     'eco': '/static/images/eco.jpg',
+    'fctr': '/static/images/fctr.jpg',
     'mc': '/static/images/mc.jpg'
 }
